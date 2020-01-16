8c1fdfb4869f369f87ed7a5991cf6989401d380b
stevenmcdonald <engineers2526@gmail.com>
2020-01-16 11:38:09 -0600

    first sample test commit

--- src/main/java/com/mt/springmongo/DemoApp.java
+++ src/main/java/com/mt/springmongo/DemoApp.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import io.kubernetes.client.ApiClient;
-import io.kubernetes.client.ApiException;
-import io.kubernetes.client.Configuration;
-import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1Pod;
-import io.kubernetes.client.models.V1PodList;
-import io.kubernetes.client.util.Config;
-
-import java.io.IOException;
 
 /**
  * Main Application controller.
@@ -25,20 +16,9 @@ import java.io.IOException;
 @ComponentScan(basePackages = "com.mt.*")
 public class DemoApp {
 
-    public static void main(String[] args) throws IOException, ApiException {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApp.class, args);
-
-        ApiClient client = Config.fromConfig("config");
-        client.setVerifyingSsl(false);
-        Configuration.setDefaultApiClient(client);
-        CoreV1Api api = new CoreV1Api();
-
-        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-        for (V1Pod item : list.getItems()) {
-            System.out.println(item.getMetadata().getName());
-        }
     }
-
 
     @RequestMapping("/")
     public String index() {
